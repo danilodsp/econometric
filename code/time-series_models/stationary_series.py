4582370f71192a31aa851e08347c8bbbb588d3ad
Danilo Pena <danilopena.ba@gmail.com>
2019-08-05 09:17:15 -0300

    Stationary series with plot

--- code/time-series_models/stationary_series.py
+++ code/time-series_models/stationary_series.py
@@ -7,21 +7,21 @@ import random as rd
 import matplotlib.pylab as plt
 
 # time
-t = np.arange(1001)
+t = 0.001 * np.arange(1001)
 
 # stationary
-c = 0.8 # constant
+c = 0.5 # constant
 sigma = 1 # N(mean, sigma)
 y_ss = np.zeros(len(t))
 for i in range(len(t)):
     y_ss[i] = c + rd.gauss(0, sigma) # stochastic
 y_sd = np.zeros(len(t))
 for i in range(1,len(t)):
-    y_sd[i] = c + 0.5*y_sd[i-1] # deterministic
+    y_sd[i] = c + 0.7*y_sd[i-1] # deterministic
 
 # non-stationary
-c = 0.8 # constant
-delta = 1
+c = 0.5 # constant
+delta = 0.2
 sigma = 1
 y_nss = np.zeros(len(t))
 for i in range(len(t)):
@@ -32,6 +32,33 @@ for i in range(1,len(t)):
 y_nsd = np.zeros(len(t))
 for i in range(len(t)):
     y_nsd[i] = c + delta*t[i] # deterministic
+
+# plots
+plt.subplot(2,2,1)
+plt.plot(t, y_ss)
+plt.title('Stationary series')
+plt.xlabel('time')
+plt.ylabel('y_ss')
+plt.axis('tight')
+
+plt.subplot(2,2,2)
+plt.plot(t, y_sd)
+plt.xlabel('time')
+plt.ylabel('y_sd')
+plt.axis('tight')
+
+plt.subplot(2,2,3)
+plt.plot(t, y_nss)
+plt.xlabel('time')
+plt.ylabel('y_nss')
+plt.axis('tight')
+
+plt.subplot(2,2,4)
+plt.plot(t, y_nsd)
+plt.xlabel('time')
+plt.ylabel('y_nsd')
+plt.axis('tight')
+plt.show()
 
 
 
